47dbb67d94eab852d0e39194bf785fb1696e7865
Wandonium <hillarywando@gmail.com>
2025-11-13 11:09:18 +0300

    Added dependencies for encrypting user passwords: bcryptjs and @types/bcryptjs and added functions for creating new user in DB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/users.schema';
 import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
+
+  async create(data: CreateUserDto) {
+    await new this.userModel({
+      ...data,
+      password: await hash(data.password, 10),
+    }).save();
+  }
 }
