b1ae062692bf0af437062092d1644b3c3286a621
Wandonium <hillarywando@gmail.com>
2025-11-14 11:29:28 +0300

    Created new jwt strategy and guard

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,26 @@
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { TokenPayload } from '../token-payload.interface';
+import { UsersService } from '../../users/users.service';
+import { Injectable } from '@nestjs/common';
 
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly userService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies?.Authentication,
+      ]),
+      secretOrKey: configService.getOrThrow('JWT_ACCESS_TOKEN_SECRET'),
+    });
+  }
+
+  async validate(payload: TokenPayload) {
+    return this.userService.getUser({ _id: payload.userId });
+  }
+}
