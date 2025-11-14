40404da6da5afd9ed607bbcb8d029c890304d361
Wandonium <hillarywando@gmail.com>
2025-11-14 11:42:04 +0300

    Added new get users route that is auth protected

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/user.decorator';
+import { User } from './schema/users.schema';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +12,12 @@ export class UsersController {
   @Post()
   async createUser(@Body() request: CreateUserDto) {
     await this.usersService.create(request);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUsers(@CurrentUser() user: User) {
+    console.log('current user: ', user);
+    return this.usersService.getUsers();
   }
 }
